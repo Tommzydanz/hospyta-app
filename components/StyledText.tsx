376e71fb56b9be4457b60c3a7915fa744e910b0c
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-25 16:01:08 +0100

    refactor: Update StyledText component to use specific font families and sizes

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -5,16 +5,38 @@ export function MonoText(props: TextProps) {
   return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
 }
 
-export function HeaderText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: "Axiforma" }]} />;
-}
-
 export function TitleText(props: TextProps) {
   return (
-    <Text {...props} style={[props.style, { fontFamily: "GothamPro_Bold" }]} />
+    <Text
+      {...props}
+      style={[{ fontFamily: "Axiforma", fontSize: 32 }, props.style]}
+    />
+  );
+}
+
+export function MediumText(props: TextProps) {
+  return (
+    <Text
+      {...props}
+      style={[{ fontFamily: "GothamPro_Medium", fontSize: 16 }, props.style]}
+    />
+  );
+}
+
+export function LightText(props: TextProps) {
+  return (
+    <Text
+      {...props}
+      style={[props.style, { fontFamily: "GothamPro_Light", fontSize: 16 }]}
+    />
   );
 }
 
 export function MsgText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: "GothamPro" }]} />;
+  return (
+    <Text
+      {...props}
+      style={[{ fontFamily: "GothamPro", fontSize: 16 }, props.style]}
+    />
+  );
 }
